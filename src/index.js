4c994c5c39365a89fe296041e1fda31976337f72
Timothy J Mahoney <38438323+Timmahoney2000@users.noreply.github.com>
2023-04-28 17:36:38 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const input = "hey you guys farted too";
+
+const vowels = ["a", "e", "i", "o", "u"];
+
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  // console.log(i);
+  if (input[i] === "e") {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === "u") {
+    resultArray.push(input[i]);
+  }
+  for (let k = 0; k < vowels.length; k++) {
+    //console.log(k);
+    if (input[i] === vowels[k]) {
+      // console.log(input[i])
+      resultArray.push(input[i]);
+    }
+  }
+}
+
+//console.log(resultArray);
+
+const resultString = resultArray.join("").toUpperCase();
+console.log(resultString);
